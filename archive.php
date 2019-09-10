e6f8c1d51ad7e43017575dd621fa8aa47e8d4b8d
mattyvac <mattyvac@gmail.com>
2019-09-10 13:47:08 -0400

    archive page edits

--- archive.php
+++ archive.php
@@ -4,21 +4,77 @@ Template Name: Archives
 */
 get_header(); ?>
  
-<div id="primary" class="site-content">
+<div id="primary" class="site-content archive">
 
 	<div id="content" role="main" class="container">
+
+	<nav class="sort-nav">
+
+		<h2>Sort Your News</h2>
+
+		<section class="sort-items">
+			<form action="/news/" method="get" id="news-archive-form">
+				<select name='category'>
+					<option value="" >All</option >
+					<?php
+					$myterms = get_terms(array('taxonomy' => 'news_category'));
+					foreach ( $myterms as $term ) {
+						if (!($term->term_id)){
+							$term = get_term_by('id', $term, 'news_category' );
+						}
+						if ( $current_term_slug == $term->slug ) {
+							$selected = 'selected="selected"';
+						} else {
+							$selected = '';
+						}
+						echo "<option value='" . $term->slug . "' $selected>" . $term->name . "</option>";
+					}
+					?>
+
+				</select >
+				<select name="years">
+					<option value="" >All</option >
+					<?php
+					for ($i=2007; $i <= date("Y"); $i++){
+						// arbitrarily choosing 2007, since that is the first news article.
+						// if we start removing old articles, we may need to dynamically calculate the oldest year with articles.
+						if ($current_year == $i){
+
+							$selected = 'selected="selected"';
+						} else {
+							$selected = '';
+						}
+						echo "<option value='" . $i . "' $selected>" . $i . "</option>";
+
+					}
+					?>
+				</select>
+
+				<button class="button" value="Find">Find</button>
+			</form>
+		</section>
+
+	</nav>
+
+	<section class="archive-content">
 	 
-	<?php while ( have_posts() ) : the_post(); ?>
-	                 
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<article>
+		                 
+				<h2 class="entry-title"><?php the_title(); ?></h2>
+				 
+				<div class="entry-content">
+				 
+					<?php the_excerpt(); ?>
+				
+				</div>
+
+			</article>
 		 
-		<div class="entry-content">
-		 
-			<?php the_excerpt(); ?>
-		
-		</div>
-	 
-	<?php endwhile; // end of the loop. ?>
+		<?php endwhile; // end of the loop. ?>
+
+	</section>
 	 
 	</div>
 
