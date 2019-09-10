5b15c6a82c726d6254d1995694dc01377c940563
Stephen Schrauger <stephen@schrauger.com>
2019-09-10 13:45:31 -0400

    fixes post count

--- includes/print-social-posts.php
+++ includes/print-social-posts.php
@@ -17,22 +17,27 @@ class print_social_posts{
         locate_template( 'includes/instagram-feed.php', TRUE, TRUE );
         locate_template( 'includes/twitter-feed.php', TRUE, TRUE );
 
-        $twitter_posts = get_transient('ucf-com-main-screen-twitter-posts');
-        if (!$twitter_posts){
-            $twitter_posts = twitterfeed::fetch_posts($count);
-            set_transient('ucf-com-main-screen-twitter-posts', $twitter_posts, WP_FS__TIME_10_MIN_IN_SEC);
-        }
+	    $twitter_posts = get_transient("ucf-com-main-screen-twitter-posts-{$count}");
+	    if (!$twitter_posts){
+		    $twitter_posts = twitterfeed::fetch_posts($count);
+		    set_transient('ucf-com-main-screen-twitter-posts', $twitter_posts, WP_FS__TIME_10_MIN_IN_SEC);
+	    }
 
-        $instagram_posts = get_transient('ucf-com-main-screen-instagram_posts');
-        if (!$instagram_posts){
-            $instagram_posts = instagramfeed::fetch_posts($count);
-            set_transient('ucf-com-main-screen-instagram_posts', $instagram_posts, WP_FS__TIME_10_MIN_IN_SEC);
-        }
+	    $instagram_posts = get_transient("ucf-com-main-screen-instagram_posts-{$count}");
+	    if (!$instagram_posts){
+		    $instagram_posts = instagramfeed::fetch_posts($count);
+		    set_transient('ucf-com-main-screen-instagram_posts', $instagram_posts, WP_FS__TIME_10_MIN_IN_SEC);
+	    }
 
         $all_posts_sorted = self::sort_posts(array_merge($twitter_posts,$instagram_posts));
         //var_dump($all_posts_sorted);
+
+	    $i = 0;
         foreach ($all_posts_sorted as $post) {
+        	$i++;
             /* @var $post socialfeedpost */
+            //echo "count is $i, and max is $count";
+            if ($i > $count) break; // our $count should be the total results, not the subtotal for each category. break once we reach our $count.
             $post->print_item();
         }
     }
@@ -62,6 +67,6 @@ class print_social_posts{
         return ($a->get_date() < $b->get_date()) ? 1 : -1;
     }
 }
-print_social_posts::output(4);
+print_social_posts::output(30);
 
 
