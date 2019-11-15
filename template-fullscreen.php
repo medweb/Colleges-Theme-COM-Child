b6068c0211de78ac947da40658facde83fe7d6bb
mattyvac <mattyvac@gmail.com>
2019-11-15 11:05:12 -0500

    social feed overflow and white overlay

--- template-fullscreen.php
+++ template-fullscreen.php
@@ -18,65 +18,10 @@
 
 	 	<?php get_template_part( 'includes/print-social-posts' ); ?>
 
+	 	<div class="overlay-white-fade">&nbsp;</div>
+
 	 </section>
 
-	 <section class="news">
-
-		<section class="container">
-
-			<section class="site-news">
-				
-				<?php get_template_part( 'includes/news-module' ); ?>
-
-			</section>
-
-			<?php if ( get_current_blog_id() == 1 ) { ?>
-
-			<section class="in-news">
-
-				<!-- Switch to blog *before* printing the url, since we want in-the-news based on blog 1. -->
-				<h4><a href="<?php bloginfo( 'url' ); ?>/news-and-communications/in-the-news/">In The News</a></h4>
-
-				<?php
-				$args = array(
-					'post_type' => 'news',
-					'posts_per_page' => 6,
-					'tax_query' => array(
-						array(
-							'taxonomy' => 'news_category',
-							'field'    => 'slug',
-							'terms'    => 'external-news',
-							'operator' => 'IN',
-						),
-					)
-				);
-
-				$the_query = new WP_Query( $args ); if ( $the_query->have_posts() ) {
-					while ( $the_query->have_posts() ) {
-						$the_query->the_post(); ?>
-
-						<article <?php post_class('innewsart'); ?>>
-
-							<a href="<?php the_field( 'affiliate_url' ); ?>" target="_blank" title="<?php the_title(); ?>" ><?php the_title(); ?>
-								<small ><?php the_date(); ?></small >
-							</a>
-
-						</article>
-
-						<?php
-					}
-				}
-				?>
-
-			</section>
-
-			<a href="<?php bloginfo( 'url' ); ?>/news/" class="more">More News</a>
-
-			<?php } ?>
-
-		</section>
-
-	</section>
 
 <?php } ?>
 
