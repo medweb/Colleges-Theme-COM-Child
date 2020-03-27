35be391bda5006789ac122bff26816cf858c5f92
Stephen Schrauger <stephen@schrauger.com>
2020-03-27 10:51:26 -0400

    validate query

--- includes/person-functions.php
+++ includes/person-functions.php
@@ -8,9 +8,12 @@
 function get_person_news_com_news_type($query) {
 
     // only run on code-defined wp_query
-    if ((!$query->is_main_query()) && (get_post_type() == 'person')){
+    if ((!$query->is_main_query()) && (get_post_type() === 'person')){
         if ($query->get('category_name') === 'faculty-news'){ // further check that the query running is for this category
-            $query->set('post_type', ['news'] ); // override the post type with our own.
+            $meta_query = $query->get('meta_query');
+            if ($meta_query[0]['key'] === 'post_associated_people'){ // we only want to modify the one single query for associated news
+	            $query->set('post_type', ['news'] ); // override the post type with our own.
+            }
         }
     }
 
