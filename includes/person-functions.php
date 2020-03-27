d9482ceeb03cc6018476b6da0bd71cf429d8df13
Stephen Schrauger <stephen@schrauger.com>
2020-03-27 13:18:40 -0400

    custom taxonomy

--- includes/person-functions.php
+++ includes/person-functions.php
@@ -9,10 +9,13 @@ function get_person_news_com_news_type($query) {
 
     // only run on code-defined wp_query
     if ((!$query->is_main_query()) && (get_post_type() === 'person')){
-        if ($query->get('category_name') === 'faculty-news'){ // further check that the query running is for this category
+        if ($query->get('category_name') === 'faculty-news' || $query->get('category_name') === 'publication'){ // further check that the query running is for this category
             $meta_query = $query->get('meta_query');
             if ($meta_query[0]['key'] === 'post_associated_people'){ // we only want to modify the one single query for associated news
-	            $query->set('post_type', ['news'] ); // override the post type with our own.
+	            $query->set('post_type', 'news' ); // override the post type with our own.
+                $query->set('taxonomy', 'news_category'); // use our custom taxonomy
+                $query->set('term', $query->get('category_name')); // copy the term
+	            $query->set('category_name', null); // can't use default category taxonomy, so clear this parameter
             }
         }
     }
