6068a1d7458689acd4fdabbf8a0d60ef1622054c
mattyvac <mattyvac@gmail.com>
2020-04-08 13:44:29 -0400

    edit style added

--- functions.php
+++ functions.php
@@ -125,6 +125,16 @@ function body_class_for_pages( $classes ) {
 
 }
 
+// Custom editor stylesheet
+add_action( 'after_setup_theme', 'com_gutenberg_css' );
+ 
+function com_gutenberg_css(){
+ 
+    add_theme_support( 'editor-styles' ); // if you don't add this line, your stylesheet won't be added
+    add_editor_style( 'editor-style.css' ); // tries to include style-editor.css directly from your theme folder
+ 
+}
+
 // Custom login screen
 add_action( 'login_head', 'custom_login_style' );
 function custom_login_style() {
