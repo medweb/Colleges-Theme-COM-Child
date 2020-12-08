a566bee4bedd843a06c02fea5f9acff742947f06
Stephen Schrauger <stephen@schrauger.com>
2020-12-08 12:18:17 -0500

    preload fonts

--- includes/preload.php
+++ includes/preload.php
@@ -13,10 +13,10 @@ add_action( 'wp_head', 'colleges_theme_com_child_preload', 5);
 
 function colleges_theme_com_child_preload(){
 	$important_fonts = array(
-		"https://med.ucf.edu/wp-content/themes/Colleges-Theme/static/fonts/ucf-sans-serif-alt/ucfsansserifalt-black-webfont.woff2",
-		"https://med.ucf.edu/wp-content/themes/Colleges-Theme/static/fonts/ucf-sans-serif-alt/ucfsansserifalt-bold-webfont.woff2",
-		"https://med.ucf.edu/wp-content/themes/Colleges-Theme/static/fonts/ucf-sans-serif-alt/ucfsansserifalt-medium-webfont.woff2",
-		"https://med.ucf.edu/wp-content/themes/Colleges-Theme/static/fonts/ucf-sans-serif-alt/ucfsansserifalt-semibold-webfont.woff2"
+		"/wp-content/themes/Colleges-Theme/static/fonts/ucf-sans-serif-alt/ucfsansserifalt-black-webfont.woff2",
+		"/wp-content/themes/Colleges-Theme/static/fonts/ucf-sans-serif-alt/ucfsansserifalt-bold-webfont.woff2",
+		"/wp-content/themes/Colleges-Theme/static/fonts/ucf-sans-serif-alt/ucfsansserifalt-medium-webfont.woff2",
+		"/wp-content/themes/Colleges-Theme/static/fonts/ucf-sans-serif-alt/ucfsansserifalt-semibold-webfont.woff2"
 	);
 	foreach ($important_fonts as $font){
 		echo "<link rel='preload' href='${font}' as='font' />";
