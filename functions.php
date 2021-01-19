5062d4c32c4fd53b4381c3dcf5ce56da65f556fb
mattyvac <mattyvac@gmail.com>
2021-01-19 09:52:30 -0500

    added menu editing snippet to function file for editor level users

--- functions.php
+++ functions.php
@@ -257,6 +257,43 @@ function get_custom_single_template($single_template) {
      return $single_template;
 }
 
+//
+// Allow editors to see access the Menus page under Appearance but hide other options
+// Note that users who know the correct path to the hidden options can still access them
+
+if ( get_current_blog_id() != '1' ) {
+
+    function hide_menu() {
+        $user = wp_get_current_user();
+        
+        // Check if the current user is an Editor
+        if ( in_array( 'editor', (array) $user->roles ) ) {
+            
+            // They're an editor, so grant the edit_theme_options capability if they don't have it
+            if ( !current_user_can( 'edit_theme_options' ) ) {
+                $role_object = get_role( 'editor' );
+                $role_object->add_cap( 'edit_theme_options' );
+            }
+            
+            // Hide the Themes page
+            remove_submenu_page( 'themes.php', 'themes.php' );
+     
+            // Hide the Widgets page
+            remove_submenu_page( 'themes.php', 'widgets.php' );
+
+            // Hide the Customize page
+            remove_submenu_page( 'themes.php', 'customize.php' );
+     
+            // Remove Customize from the Appearance submenu
+            global $submenu;
+            unset($submenu['themes.php'][6]);
+        }
+    }
+     
+    add_action('admin_menu', 'hide_menu', 10);
+
+}
+
 // Run environment options and functions
 switch ( ENVIRONMENT ):
     case 'local':
