bdf2f79003c454437ccde0cd0815623733e13db7
Matthew Vaccaro <mattyvac@gmail.com>
2022-11-28 14:14:44 -0500

    commented out old analytics

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -4,7 +4,7 @@
  */
 <!-- Google Analytics -->
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+/*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
@@ -28,7 +28,7 @@ ga('create', {
         cookieDomain: 'med.ucf.edu',
         name: 'clientCOMLibrary' // COM Library Study
 });
-ga('clientCOMLibrary.send', 'pageview');
+ga('clientCOMLibrary.send', 'pageview');*/
 
 <!-- End Google Analytics -->
 
