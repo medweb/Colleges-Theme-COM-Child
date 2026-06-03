7972d705bb40ecde683641ea57cb2d44aa313183
HashBangCrash <115586744+HashBangCrash@users.noreply.github.com>
2026-06-03 09:53:11 -0400

    disable full-size image option for non admins

---
+++ includes/disable-image-sizes.php
@@ -0,0 +1,16 @@
+/**
+ * Remove the Full Size image option for non-admin users.
+ *
+ * @param array $sizes Available image sizes.
+ * @return array
+ */
+function com_child_theme_restrict_image_sizes( $sizes ) {
+
+    if ( ! current_user_can( 'manage_options' ) ) {
+        unset( $sizes['full'] );
+    }
+
+    return $sizes;
+}
+
+add_filter( 'image_size_names_choose', 'mytheme_restrict_image_sizes' );
