0b44e32a13ba6b5db02f11c9ce76ce83823f1bde
HashBangCrash <115586744+HashBangCrash@users.noreply.github.com>
2026-08-05 16:43:01 -0400

    add publishpress capability

--- includes/disable-image-sizes.php
+++ includes/disable-image-sizes.php
@@ -1,16 +1,44 @@
 <?php
+
+const COM_USE_FULL_IMAGE_SIZE = 'com_use_full_image_size';
+
+
 /**
- * Remove the Full Size image option for non-admin users.
+ * Restrict the Full Size image option to only admins or those
+ * specifically granted the capability with publishpress.
  *
  * @param array $sizes Available image sizes.
  * @return array
  */
 function com_child_theme_restrict_image_sizes( $sizes ) {
 
-    if ( ! current_user_can( 'manage_options' ) ) {
-        unset( $sizes['full'] );
-    }
-    return $sizes;
+	if (
+		current_user_can( 'manage_options' ) ||
+		current_user_can( COM_USE_FULL_IMAGE_SIZE )
+	) {
+		return $sizes;
+	}
+
+	unset( $sizes['full'] );
+
+	return $sizes;
 }
 
 add_filter( 'image_size_names_choose', 'com_child_theme_restrict_image_sizes' );
+
+/**
+ * Register our custom capabilities with PublishPress Capabilities.
+ *
+ * @param array $plugin_caps Existing plugin capabilities.
+ * @return array
+ */
+function com_child_theme_restrict_image_sizes_register_capabilities( $plugin_caps ) {
+
+	$plugin_caps['Colleges Theme COM Child'] = array(
+		COM_USE_FULL_IMAGE_SIZE => 'use "Full Size" image size',
+	);
+
+	return $plugin_caps;
+}
+
+add_filter( 'cme_plugin_capabilities', 'com_child_theme_restrict_image_sizes_register_capabilities' );
